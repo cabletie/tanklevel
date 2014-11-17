7fb5da533720d0fef37482cc9a63f6ee606ef45b
Peter Westley <peter@westley.id.au>
2014-11-17 21:19:50 +1100

    tryin gto get threaded timer to do the outputting

--- tanklevel.py
+++ tanklevel.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
-from ABElectronics_ADCPi import ADCPi
 import datetime
 import time
 import signal
 import sys
 import math
+import threading
 
 # ================================================
 # Based on
@@ -18,11 +18,20 @@ import math
 
 # Setup to gracefully catch ^C and exit
 def signal_handler(signal, frame):
-        print rawReadings
-        print('\nExiting')
+	print >> sys.stderr, rawReadings
+        print >> sys.stderr,'\nExiting'
         sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
+
+sys.path.append('../ABElectronics_Python_Libraries/ABElectronics_ADCPi')
+from ABElectronics_ADCPi import ADCPi
+
+# Emit the current datetime and sample average
+def emit():
+  threading.Timer(5.0, emit, rawReadings).start()
+  a = math.fsum(rawReadings)/len(rawReadings)
+  print ("{},{:04f},{:04f}".format(str(datetime.datetime.utcnow()),a,v))
 
 # Initialise the ADC device using the default addresses and sample rate, change this value if you have changed the address selection jumpers
 # Sample rate can be 12,14, 16 or 18
@@ -30,14 +39,15 @@ adc = ADCPi(0x68, 0x69, 18)
 
 # Initialise the raw readings array
 rawReadings = [] 
-currentIndex = 0
+timerNotStarted = True
+
+emit()
 
 while True:
   v = adc.readVoltage(8)
+  if timerNotStarted and len(rawReadings) > 10: 
+    emit()
+    timerNotStarted = False
   if len(rawReadings) > 60:
-    rawReadings[currentIndex] = v
-  else :
-    rawReadings.append(v)
-  #print ("Current: %04f" % v)
-  a = math.fsum(rawReadings)/len(rawReadings)
-  print ("{:04f},{:04f}".format(a,v))
+    del rawReadings[0]
+  rawReadings.append(v)
