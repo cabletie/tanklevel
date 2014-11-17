db76e0430a686d9509c0b51efd48ae476bac8f69
Peter Westley <peter@westley.id.au>
2014-11-17 21:39:18 +1100

    Now working with http://stackoverflow.com/users/624066/mestrelion solution from https://stackoverflow.com/a/13151299/4260773

--- tanklevel.py
+++ tanklevel.py
@@ -5,7 +5,7 @@ import time
 import signal
 import sys
 import math
-import threading
+from threading import Timer
 
 # ================================================
 # Based on
@@ -27,27 +27,51 @@ signal.signal(signal.SIGINT, signal_handler)
 sys.path.append('../ABElectronics_Python_Libraries/ABElectronics_ADCPi')
 from ABElectronics_ADCPi import ADCPi
 
-# Emit the current datetime and sample average
-def emit():
-  threading.Timer(5.0, emit, rawReadings).start()
-  a = math.fsum(rawReadings)/len(rawReadings)
-  print ("{},{:04f},{:04f}".format(str(datetime.datetime.utcnow()),a,v))
-
 # Initialise the ADC device using the default addresses and sample rate, change this value if you have changed the address selection jumpers
 # Sample rate can be 12,14, 16 or 18
 adc = ADCPi(0x68, 0x69, 18)
+
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+# Emit the current datetime and sample average
+def emit(rawReadings):
+  a = math.fsum(rawReadings)/len(rawReadings)
+  print ("{},{:04f}".format(str(datetime.datetime.utcnow()),a))
 
 # Initialise the raw readings array
 rawReadings = [] 
 timerNotStarted = True
 
-emit()
+rt = RepeatedTimer(60, emit, rawReadings)
 
-while True:
-  v = adc.readVoltage(8)
-  if timerNotStarted and len(rawReadings) > 10: 
-    emit()
-    timerNotStarted = False
-  if len(rawReadings) > 60:
-    del rawReadings[0]
-  rawReadings.append(v)
+try:
+  while True:
+    v = adc.readVoltage(8)
+    if len(rawReadings) > 60:
+      del rawReadings[0]
+    rawReadings.append(v)
+finally:
+  rt.stop()
