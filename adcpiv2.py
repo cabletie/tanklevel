aa3c21e6b76d4098cd6c32be1f9436e085c8bb90
Peter Westley <peter@westley.id.au>
2014-12-17 23:38:45 +1100

    Removed code to find IP address

--- adcpiv2.py
+++ adcpiv2.py
@@ -17,30 +17,6 @@ import csv
 import logging
 import configparser
 
-# Get the IP address from the current network interface
-if os.name != "nt":
-    import fcntl
-    import struct
-    def get_interface_ip(ifname):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        return socket.inet_ntoa(fcntl.ioctl(
-                s.fileno(),
-                0x8915,  # SIOCGIFADDR
-                struct.pack('256s', ifname[:15])
-            )[20:24])
-
-def get_lan_ip():
-    ip = socket.gethostbyname(socket.gethostname())
-    if ip.startswith("127.") and os.name != "nt":
-        interfaces = ["eth0","eth1","eth2","wlan0","wlan1","wifi0","ath0","ath1","ppp0"]
-        for ifname in interfaces:
-            try:
-                ip = get_interface_ip(ifname)
-                break;
-            except IOError:
-                pass
-    return ip
-
 # Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-V", "--debug", help="Turn on debugging to stderr", action="store_true")
